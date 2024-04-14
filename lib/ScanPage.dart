4d033f8e4315952b1bdf0f68e4c6664c178aecb5
Bence Zsolt Beregi <dqk6te@INF.ELTE.HU>
2024-04-14 17:36:18 +0200

    feature/database: Working barcode scanner and data fetch

--- lib/ScanPage.dart
+++ lib/ScanPage.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+
+import 'ApiClient.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({Key? key}) : super(key: key);
@@ -12,6 +16,7 @@ class _ScanPageState extends State<ScanPage> {
   String result = "None";
   MobileScannerController cameraController = new MobileScannerController();
   bool _screenOpened = false;
+  var api = new ApiClient();
 
   @override
   Widget build(BuildContext context) {
@@ -22,29 +27,54 @@ class _ScanPageState extends State<ScanPage> {
         child: Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.white),
             ),
             width: 400,
             height: 200,
-            child: MobileScanner(
-              fit: BoxFit.cover,
-              controller: cameraController,
-              onDetect: _foundBarcode,
-            )),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: MobileScanner(
+                fit: BoxFit.cover,
+                controller: cameraController,
+                onDetect: _foundBarcode,
+            ))),
       ),
     );
   }
 
-  void _foundBarcode(BarcodeCapture barcode) {
+  void _foundBarcode(BarcodeCapture barcode) async {
     if (!_screenOpened) {
       final String code = barcode.barcodes[0].rawValue ?? "---";
       debugPrint("Barcode found: $code");
       _screenOpened = true;
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) =>
-                FoundCodeScreen(screenClosed: _screenWasClosed, value: code),
-          ));
+
+      var scanned_product = null;
+      try {
+        var response = await api.getRequest('product/$code');
+        if (response.statusCode == 200) {
+          var product = json.decode(response.body);
+          setState(() {
+            scanned_product = product;
+          });
+          debugPrint(product["image_url"]);
+        } else {
+          debugPrint('Failed to fetch product: ${response.statusCode}');
+        }
+      } catch (e) {
+        debugPrint('Exception while fetching users: $e');
+      }
+
+      if( scanned_product != null) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  FoundCodeScreen(screenClosed: _screenWasClosed, value: scanned_product),
+            ));
+      } else {
+        _screenOpened = false;
+        debugPrint("No id found!");
+      }
     }
   }
 
@@ -54,7 +84,7 @@ class _ScanPageState extends State<ScanPage> {
 }
 
 class FoundCodeScreen extends StatefulWidget {
-  final String value;
+  final Map<String, dynamic> value;
   final Function() screenClosed;
 
   const FoundCodeScreen({
@@ -68,18 +98,24 @@ class FoundCodeScreen extends StatefulWidget {
 }
 
 class _FoundCodeScreenState extends State<FoundCodeScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(int.parse('0xFF1b212f')),
       appBar: AppBar(
-        title: Text("Found Code"),
+        backgroundColor: Color(int.parse('0xFF1b212f')),
+        title: Text("Found Item", style: TextStyle(color: Color(int.parse('0xFF39EBB1'))),),
         centerTitle: true,
         leading: IconButton(
           onPressed: () {
             widget.screenClosed();
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back_ios_new_outlined),
+          icon: Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Color(int.parse('0xFF39EBB1'))
+          ),
         ),
       ),
       body: Center(
@@ -88,19 +124,169 @@ class _FoundCodeScreenState extends State<FoundCodeScreen> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.network(
+                  widget.value["image_url"],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               Text(
-                "Scanned Code:",
+                widget.value["name"],
                 style: TextStyle(
+                  color: Colors.white,
                   fontSize: 20,
                 ),
               ),
               SizedBox(
                 height: 20,
               ),
-              Text(
-                widget.value,
-                style: TextStyle(fontSize: 16),
-              )
+              Table(
+                children: [
+                  TableRow(
+                    children: [
+                      TableCell(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Vegan", style: TextStyle(color: Colors.white, fontSize: 14)),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 10,
+                              height: 10,
+                               decoration: BoxDecoration(
+                                 shape: BoxShape.circle,
+                                 color: widget.value["isVegan"] == 1 ? Colors.green : Colors.red,
+                               ),
+                            )
+                          ],
+                        ),
+                    ),
+                    TableCell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Gluten", style: TextStyle(color: Colors.white, fontSize: 14)),
+                          SizedBox(width: 8),
+                          Container(
+                            width: 10,
+                            height: 10,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: widget.value["isGlutenFree"] == 1 ? Colors.green : Colors.red,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                    ]
+                  ),
+                  TableRow(
+                      children: [
+                        TableCell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Dairy", style: TextStyle(color: Colors.white, fontSize: 14)),
+                              SizedBox(width: 8),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: widget.value["isDiaryFree"] == 1 ? Colors.green : Colors.red,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        TableCell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Nuts", style: TextStyle(color: Colors.white, fontSize: 14)),
+                              SizedBox(width: 8),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: widget.value["isNutFree"] == 1 ? Colors.green : Colors.red,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ]
+                  ),
+                  TableRow(
+                      children: [
+                        TableCell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Vegetarian", style: TextStyle(color: Colors.white, fontSize: 14)),
+                              SizedBox(width: 8),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: widget.value["isVegetarian"] == 1 ? Colors.green : Colors.red,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        TableCell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Halal", style: TextStyle(color: Colors.white, fontSize: 14)),
+                              SizedBox(width: 8),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: widget.value["isHalal"] == 1 ? Colors.green : Colors.red,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ]
+                  ),
+                  TableRow(
+                      children: [
+                        TableCell(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Kosher", style: TextStyle(color: Colors.white, fontSize: 14)),
+                              SizedBox(width: 8),
+                              Container(
+                                width: 10,
+                                height: 10,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: widget.value["isKosher"] == 1 ? Colors.green : Colors.red,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        TableCell(
+                          child: Container(
+                            width: 0,
+                          ),
+                        ),
+                      ]
+                  )
+                ],
+              ),
             ],
           ),
         ),
@@ -108,3 +294,4 @@ class _FoundCodeScreenState extends State<FoundCodeScreen> {
     );
   }
 }
+
